4ecee6c2b029ce7f762d2741a731f979869a5210
pennyyi97 <pennyyi97@gmail.com>
2017-09-19 12:14:58 +0900

    first ver.

--- app/src/main/java/com/example/sec/sep12/Main2Activity.java
+++ app/src/main/java/com/example/sec/sep12/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.sec.sep12;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,9 +14,24 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Log.i("DEBUG","Hello at the Main2 Activity");
+
+    }
+
+
+
+
+    public void Gotoback (View ddd){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
     public void NextScene (View bbb){
         Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+    public void Goto3 (View ddd){
+        Intent i = new Intent(this, Main3Activity.class);
         startActivity(i);
     }
     public void onClick(View v) {
--- app/src/main/java/com/example/sec/sep12/MainActivity.java
+++ app/src/main/java/com/example/sec/sep12/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sec.sep12;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //내가 만든 view componant 의 뭐라고...?ㅜㅜ
+
+        Log.i("DEBUG","Hello");
     }
 
 
